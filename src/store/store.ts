d16c7052047245f1fd119e7138632f553c6b2f2c
Muhammad Faisal Affan <faisallionel@gmail.com>
2022-11-24 15:30:18 +0700

    feat: update

---
+++ src/store/store.ts
@@ -0,0 +1,18 @@
+// import { useReducer } from "react";
+
+// export type TodoItem = {
+//     text: string;
+//     checked: boolean;
+// }
+
+// export function useTodoItems(defaultItems: TodoItem[]) {
+//     function reducer(state: TodoItem) {
+//         return state;
+//     }
+//     return useReducer(reducer);
+// }
+const reducer = () => {
+
+}
+
+export default reducer
---
+++ src/utils/Config.ts
@@ -0,0 +1,4 @@
+const Config = {
+    APIToken: 'aef0da6336ec93a02fca46470fa217de9e6f3f3c7cd67466a7068804d01131f0'
+}
+export default Config
